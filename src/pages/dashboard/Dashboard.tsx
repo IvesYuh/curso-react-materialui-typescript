e07da5d96600c52cb1c236365f7ed1d31d8db3f2
Ives Yuh <IvesYuuh@gmail.com>
2025-05-07 23:38:53 -0300

    feat: Finalizado layout base responsivo

---
+++ src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,11 @@
+import { LayoutBaseDePagina } from "../../shared/layouts";
+
+   
+   
+   export const DashBoard = () => {
+        return(
+            <LayoutBaseDePagina titulo='Página Inicial' barraDeFerramentas={<>Barra de ferramentas</>}>
+                Testando
+            </LayoutBaseDePagina>
+        );
+    };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@mui/material";
+
 import { Routes, Route, Navigate } from "react-router-dom";
-import { useAppThemeContext, useDrawerContext } from "../shared/contexts";
+import { useDrawerContext } from "../shared/contexts";
 import { useEffect } from "react";
+import { DashBoard } from "../pages";
 
 export const AppRoutes = () => {
-    const { toggleDrawerOpen, setDrawerOption } = useDrawerContext();
+    const { setDrawerOption } = useDrawerContext();
 
     useEffect(() => {
         setDrawerOption([
@@ -18,7 +19,7 @@ export const AppRoutes = () => {
 
     return(
         <Routes>
-            <Route path="/pagina-inicial" element={<Button variant="contained" color="primary" onClick={toggleDrawerOpen}>toggle drawer</Button>} />
+            <Route path="/pagina-inicial" element={<DashBoard />} />
 
             <Route path="*" element={<Navigate to="/pagina-inicial" />}/>
         </Routes>
