a6f5023b444a86dcfca4cdb834074b6d938245a5
Ives Yuh <IvesYuuh@gmail.com>
2025-05-27 21:34:00 -0300

    feat: Implementado alteração de cores do tipografia.

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,6 +1,6 @@
-import { Avatar, Box, Divider, Drawer, useTheme, List, ListItemButton,ListItemIcon, ListItemText, Icon, useMediaQuery } from "@mui/material";
+import { Avatar, Box, Divider, Drawer, useTheme, List, ListItemButton, ListItemIcon, ListItemText, Icon, useMediaQuery } from "@mui/material";
 import React, { PropsWithChildren } from "react";
-import { useDrawerContext } from "../../contexts";
+import { useAppThemeContext, useDrawerContext } from "../../contexts";
 import { useMatch, useNavigate, useResolvedPath } from "react-router-dom";
 
 interface IListItemLinkProps {
@@ -10,19 +10,19 @@ interface IListItemLinkProps {
     onClick: (() => void) | undefined;
 }
 
-const ListItemLink: React.FC<IListItemLinkProps> =  ({ to, icon, label, onClick }) => {
+const ListItemLink: React.FC<IListItemLinkProps> = ({ to, icon, label, onClick }) => {
 
     const navigate = useNavigate();
 
     const resolvedPath = useResolvedPath(to);
-    const match = useMatch({path: resolvedPath.pathname, end: false});
+    const match = useMatch({ path: resolvedPath.pathname, end: false });
 
     const handleClick = () => {
         navigate(to);
         onClick?.();
     }
 
-    return(
+    return (
         <ListItemButton selected={!!match} onClick={handleClick}>
             <ListItemIcon>
                 <Icon>icon</Icon>
@@ -37,6 +37,7 @@ export const MenuLateral: React.FC<PropsWithChildren<{}>> = ({ children }) => {
     const smDown = useMediaQuery(theme.breakpoints.down('sm'));
 
     const { isDrawerOpen, toggleDrawerOpen, drawerOptions } = useDrawerContext();
+    const { toggleTheme } = useAppThemeContext();
 
     return (
         <>
@@ -44,9 +45,9 @@ export const MenuLateral: React.FC<PropsWithChildren<{}>> = ({ children }) => {
                 <Box width={theme.spacing(28)} height={"100%"} display={"flex"} flexDirection={"column"} >
 
                     <Box width={"100%"} height={theme.spacing(20)} display={"flex"} alignItems={"center"} justifyContent={"center"}>
-                    <Avatar 
-                        sx={{ height: theme.spacing(12), width: theme.spacing(12)}}
-                        alt="X" src="/static/images/avatar/1.jpg" />
+                        <Avatar
+                            sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
+                            alt="X" src="/static/images/avatar/1.jpg" />
                     </Box>
 
                     <Divider />
@@ -56,13 +57,24 @@ export const MenuLateral: React.FC<PropsWithChildren<{}>> = ({ children }) => {
                         <List component="nav">
                             {drawerOptions.map(drawerOptions => (
                                 <ListItemLink
-                                key={drawerOptions.path}
-                                icon={drawerOptions.icon}
-                                to={drawerOptions.path}
-                                label={drawerOptions.label}
-                                onClick={smDown ? toggleDrawerOpen : undefined}
+                                    key={drawerOptions.path}
+                                    icon={drawerOptions.icon}
+                                    to={drawerOptions.path}
+                                    label={drawerOptions.label}
+                                    onClick={smDown ? toggleDrawerOpen : undefined}
                                 />
                             ))}
+                        </List>
+                    </Box>
+
+                    <Box>
+                        <List component="nav">
+                            <ListItemButton onClick={toggleTheme}>
+                                <ListItemIcon>
+                                    <Icon>dark_mode</Icon>
+                                </ListItemIcon>
+                                <ListItemText primary={"Alternar Tema"} />
+                            </ListItemButton>
                         </List>
 
                     </Box>
@@ -71,7 +83,7 @@ export const MenuLateral: React.FC<PropsWithChildren<{}>> = ({ children }) => {
             </Drawer>
 
             <Box height={"100vh"} marginLeft={smDown ? 0 : theme.spacing(28)}>
-            {children}
+                {children}
             </Box>
         </>
     );
