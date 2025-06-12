6d9802d9aecdfada8543a637f9c845a2e34623b2
Ives Yuh <IvesYuuh@gmail.com>
2025-06-11 23:22:06 -0300

    feat: Configuração variáveis ambiente no React.

--- src/shared/services/api/axios-config/index.ts
+++ src/shared/services/api/axios-config/index.ts
@@ -1,1 +1,15 @@
-  
+  import axios from "axios";
+import { responseInterceptor } from "./interceptors";
+import { error } from "console";
+import { Environment } from "../../../environment";
+
+  const Api = axios.create({
+    baseURL: Environment.URL_BASE,
+  });
+
+  Api.interceptors.response.use(
+    (response) => responseInterceptor(response),
+    (error) => responseInterceptor(error)
+  );
+
+  export { Api };
